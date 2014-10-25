4e983425c53278dc1aba56745a0623ac847fab12
D <dav.weiser@googlemail.com>
2014-10-26 00:22:08 +0200

    closes status bar panel after button click

--- src/eu/musesproject/predictionclient/CorrectionReceiver.java
+++ src/eu/musesproject/predictionclient/CorrectionReceiver.java
@@ -10,6 +10,9 @@ public class CorrectionReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		Toast.makeText(context, "Not implemented. Coming soon...", Toast.LENGTH_LONG).show();
+		
+		Intent it = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+		context.sendBroadcast(it);
 	}
 
 }
